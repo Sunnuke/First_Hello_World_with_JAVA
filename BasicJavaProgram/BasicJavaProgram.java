a53e8bffd70a8a9b5fec24f4e1b009394e9b4caa
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-24 19:45:14 -0800

    Added println function with string value

--- BasicJavaProgram/BasicJavaProgram.java
+++ BasicJavaProgram/BasicJavaProgram.java
@@ -1,5 +1,5 @@
 public class BasicJavaProgram {
     public static void main(String[] args) {
-        
+        System.out.println("My name is Javier. I am 22 years old. My home state is Maryland.");
     }
 }
