42e506c037b373dca3e1ea7c5db87b59890dd73b
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-25 17:35:16 -0800

    Added variables, and assigned them values of age, name, and homeState

--- BasicJavaProgram/BasicJavaProgram.java
+++ BasicJavaProgram/BasicJavaProgram.java
@@ -1,5 +1,8 @@
 public class BasicJavaProgram {
     public static void main(String[] args) {
+        String name = "Javier"
+        int age = 22
+        String homeState = "Maryland"
         System.out.println("My name is Javier. I am 22 years old. My home state is Maryland.");
     }
 }
