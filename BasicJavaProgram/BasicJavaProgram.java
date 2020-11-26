d5f56939d1afcf3389d455675abf69089b04efa3
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-25 17:39:48 -0800

    Compiled BasicJavaProgram.java

--- BasicJavaProgram/BasicJavaProgram.java
+++ BasicJavaProgram/BasicJavaProgram.java
@@ -1,8 +1,8 @@
 public class BasicJavaProgram {
     public static void main(String[] args) {
-        String name = "Javier"
-        int age = 22
-        String homeState = "Maryland"
+        String name = "Javier";
+        int age = 22;
+        String homeState = "Maryland";
         System.out.println("My name is " + name +". I am " + age + " years old. My home state is " + homeState + ".");
     }
 }
